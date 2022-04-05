2bf2ce363804364252281bb7a57aae7402c4cc12
Vahtee Boo <vahtee.boo@gmail.com>
2022-04-05 22:22:02 +0300

    Initial Commit

---
+++ SubmitTextFieldSTBC/SubmitTextFieldSTBCApp.swift
@@ -0,0 +1,17 @@
+//
+//  SubmitTextFieldSTBCApp.swift
+//  SubmitTextFieldSTBC
+//
+//  Created by Vahtee Boo on 05.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct SubmitTextFieldSTBCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
