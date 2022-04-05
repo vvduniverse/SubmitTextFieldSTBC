6d7e07e5dd10533c1a766f3bba1d1460af867443
Vahtee Boo <vahtee.boo@gmail.com>
2022-04-05 22:23:33 +0300

    done

--- SubmitTextFieldSTBC/ContentView.swift
+++ SubmitTextFieldSTBC/ContentView.swift
@@ -5,12 +5,62 @@
 //  Created by Vahtee Boo on 05.04.2022.
 //
 
+
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var text = ""
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            TextField("Placeholder...", text: $text)
+                .submitLabel(.continue)
+                .onSubmit {
+                    print("Some button pressed")
+                }
+            
+            TextField("Placeholder...", text: $text)
+                .submitLabel(.go)
+                .onSubmit {
+                    print("Some button pressed")
+                }
+            
+            TextField("Placeholder...", text: $text)
+                .submitLabel(.done)
+                .onSubmit {
+                    print("Some button pressed")
+                }
+            
+            TextField("Placeholder...", text: $text)
+                .submitLabel(.next)
+                .onSubmit {
+                    print("Some button pressed")
+                }
+            
+            TextField("Placeholder...", text: $text)
+                .submitLabel(.search)
+                .onSubmit {
+                    print("Some button pressed")
+                }
+            
+            TextField("Placeholder...", text: $text)
+                .submitLabel(.route)
+                .onSubmit {
+                    print("Some button pressed")
+                }
+            
+            TextField("Placeholder...", text: $text)
+                .submitLabel(.send)
+                .onSubmit {
+                    print("Some button pressed")
+                }
+            
+            TextField("Placeholder...", text: $text)
+                .submitLabel(.join)
+                .onSubmit {
+                    print("Some button pressed")
+                }
+        }
     }
 }
 
